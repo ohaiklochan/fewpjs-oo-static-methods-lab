ec1da05378b4a83a8167c1fee26180c2c83fbc11
ohaiklochan <caforonda103@yahoo.com>
2021-04-01 16:41:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    string = string[0].toUpperCase()+string.slice(1);
+    let sentence = string.split(" ");
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    for(let i = 0; i < sentence.length; i++) {
+      if(!(exceptions.includes(sentence[i]))) {
+        sentence[i] = sentence[i][0].toUpperCase()+sentence[i].slice(1);
+      }
+    }
+    return sentence.join(" ")
+  }
 }
+
